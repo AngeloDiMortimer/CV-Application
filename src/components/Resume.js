f4d998a11b5788e24f6a0d10f79e2d1af9775392
AngeloDiMortimer <70825802+AngeloDiMortimer@users.noreply.github.com>
2023-06-07 16:17:39 -0400

    Created Resume component

---
+++ src/components/Resume.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Aside from "./Aside";
+
+class Resume extends Component {
+    render() {
+        return (
+            <div id="Resume">
+                <Aside />
+            </div>
+        );
+    };
+}
